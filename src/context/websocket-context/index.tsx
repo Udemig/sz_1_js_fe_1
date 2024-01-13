3dc4957cac15cfdc9b00229428a174a8c6e427f9
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2024-01-13 16:15:18 +0300

    websocket

--- src/context/websocket-context/index.tsx
+++ src/context/websocket-context/index.tsx
@@ -1,4 +1,7 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { showSwal } from "../../utils/functions";
 
 export type WebsocketContextValueType = {};
 
@@ -8,37 +11,72 @@ export type WebsocketContextPropsType = {
 
 const WebsocketContextProvider = createContext<WebsocketContextValueType>({});
 
+const websocket = new WebSocket(import.meta.env.VITE_CHAT_API_WEBSOCKET_URL);
+
 export default function WebsocketContext(props: WebsocketContextPropsType) {
-  const [websocket, setWebsocket] = useState<WebSocket>(
-    new WebSocket(import.meta.env.VITE_CHAT_API_WEBSOCKET_URL)
-  );
+  const authState = useSelector((state: RootState) => state.authState);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isLoggedIn && websocket.readyState === WebSocket.OPEN) {
+      websocket.send(
+        JSON.stringify({
+          command: "auth/login",
+          token: authState.token,
+        })
+      );
+      setIsLoggedIn(true);
+    }
+  }, [authState.user]);
 
   const contextValue: WebsocketContextValueType = {};
 
+  const onWebsocketOpen = (event: Event) => {
+    console.log(">> 🚀 event:", event);
+    console.log(">> websocket open event triggered.");
+
+    websocket.send(
+      JSON.stringify({
+        hb: Date.now(),
+      })
+    );
+  };
+
+  const onWebsocketClose = (event: CloseEvent) => {
+    console.log(">> 🚀 event:", event);
+    console.log("Remote connection closed.");
+
+    showSwal("error", "Websocket bağlantısı koptu, sayfayı yenileyiniz.");
+
+    // TODO Fix reconnect problem.
+  };
+
+  const onWebsocketMessage = (event: MessageEvent<any>) => {
+    console.log(">> 🚀 event:", event);
+    console.log("Message from server ", event.data);
+  };
+
+  const onWebsocketError = (event: Event) => {
+    console.log(">> 🚀 event:", event);
+    console.log("Remote connection error occured:", event);
+  };
+
   useEffect(() => {
-    websocket.addEventListener("open", (event) => {
-      websocket.send(
-        JSON.stringify({
-          // hb: heartbeat
-          hb: Date.now(),
-        })
-      );
-    });
+    websocket.addEventListener("open", onWebsocketOpen);
+    websocket.addEventListener("close", onWebsocketClose);
+    websocket.addEventListener("error", onWebsocketError);
 
-    websocket.addEventListener("message", (event) => {
-      console.log("Message from server ", event.data);
-    });
-
-    websocket.addEventListener("close", (event) => {
-      console.log("Remote connection closed.");
-    });
-
-    websocket.addEventListener("error", (event) => {
-      console.log("Remote connection error occured:", event);
-    });
+    websocket.addEventListener("message", onWebsocketMessage);
 
     return () => {
-      websocket.close();
+      console.log(">> websocket context unmount executed.");
+      //websocket.close();
+
+      websocket.removeEventListener("open", onWebsocketOpen);
+      websocket.removeEventListener("close", onWebsocketOpen);
+      websocket.removeEventListener("error", onWebsocketOpen);
+
+      websocket.removeEventListener("message", onWebsocketOpen);
     };
   }, []);
 
